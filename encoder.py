67f7588b3429b1de882262092a6cdedac6bcc8c5
bntly-hckpck <bntly_dev@pm.me>
2026-08-22 10:26:48 +0000

    Update encoder.py
    converted from rgb to rgba to maintain transparency, without it the output loses its alpha channel and appears visually different from the input (if previously transparent)

--- encoder.py
+++ encoder.py
@@ -16,8 +16,8 @@ def bit_conversion(text):
 def encode_message(input_path, secret_message, output_path):
 
     image = Image.open(input_path) # open the input image
-    rgb_image = image.convert('RGB') # convert to rgb format, discarding alpha channel if present
-    pixels = list(rgb_image.getdata()) # get all of the pixels data
+    rgba_image = image.convert('RGBA') # convert to rgba format
+    pixels = list(rgba_image.getdata()) # get all of the pixels data
 
     bit_capacity = len(pixels) * 3 # total hiding capacity (1 pixel = 3 bits), if message doesn't fit data gets corrupted
     encoded_message = bit_conversion(secret_message) + ('0' * 16) # creates encoded message, converts and adds 16 0 bits delimiter 
@@ -26,7 +26,7 @@ def encode_message(input_path, secret_message, output_path):
 
     for i in range(len(pixels)): # main loop, will iterate over every pixel of the image
 
-        r, g, b = pixels[i] # extracts the rgb values of the current [i] pixel 
+        r, g, b, a = pixels[i] # extracts the rgba values of the current [i] pixel, but we'll be using only r, g and b
         
         if bit_idx < len(encoded_message): # if we still have bits to hide, proceed
             hidden_bit = encoded_message[bit_idx] # reads the current bit from the secret message
@@ -43,16 +43,16 @@ def encode_message(input_path, secret_message, output_path):
             b = (b & 0xFE) | int(hidden_bit)
             bit_idx += 1
         
-        pixels[i] = (r, g, b) # assigns modified bits to [i] pos in pixels list, inserting hidden message
+        pixels[i] = (r, g, b,a) # assigns modified bits to [i] pos in pixels list, inserting hidden message
 
-    encoded_image = Image.new('RGB', rgb_image.size) # creating a new image for the output
+    encoded_image = Image.new('RGBA', rgba_image.size) # creating a new rgba image for the output
     encoded_image.putdata(pixels) # inserting every pixel into the new image
     encoded_image.save(output_path) # saving the new, encoded image, to the desired output path (see below)
 
 # executing the program
 if __name__ == '__main__':
 
-    input_path = 'encoding_input.png' # here goes your image's input path
+    input_path = 'bentley.png' # here goes your image's input path
     secret_message = "this is my secret message" # here goes your secret message
     output_path = 'encoding_output.png' # here goes your desired image's output path
 
